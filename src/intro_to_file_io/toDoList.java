e581f9db8f56d097ff7bbbff60f2e161d87d2475
Garrett-Thomas <league@WTS5.attlocal.net>
2017-11-05 12:49:41 -0800

    asd
    sad

--- src/intro_to_file_io/toDoList.java
+++ src/intro_to_file_io/toDoList.java
@@ -2,10 +2,13 @@ package intro_to_file_io;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,7 +23,10 @@ public class toDoList implements ActionListener {
 	JButton add;
 	JButton remove;
 	JButton load;
+	JButton save;
 	public String tet;
+	ArrayList<String> names;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		toDoList a = new toDoList();
@@ -28,12 +34,15 @@ public class toDoList implements ActionListener {
 	}
 
 	void activate() {
+		names = new ArrayList<String>();
 		fram = new JFrame();
 		pan = new JPanel();
 		add = new JButton();
 		remove = new JButton();
 		load = new JButton();
+		save = new JButton();
 		fram.add(pan);
+		pan.add(save);
 		pan.add(add);
 		pan.add(remove);
 		pan.add(load);
@@ -41,52 +50,61 @@ public class toDoList implements ActionListener {
 		remove.setText("remove");
 		load.setText("load");
 		fram.pack();
+		save.setText("save");
 		fram.setVisible(true);
+		save.addActionListener(this);
 		add.addActionListener(this);
 		remove.addActionListener(this);
 		load.addActionListener(this);
 		fram.setDefaultCloseOperation(3);
-		
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 		if (e.getSource() == add) {
-			 tet = JOptionPane.showInputDialog("Type some text");
+			tet = JOptionPane.showInputDialog("Type some text");
+			names.add(tet);
+			System.out.println(names);
+		} else if (e.getSource() == remove) {
+			if (names != null) {
+				names.remove(names.size() - 1);
+				System.out.println(names);
+			}
+		} else if (e.getSource() == save) {
 			try {
-				FileWriter fw = new FileWriter("src/intro_to_file_io/test4.txt", true);
-				fw.write("\n" + tet);
+				FileWriter fw = new FileWriter("src/intro_to_file_io/test4.txt");
+			for (int i = 0; i < names.size(); i++) {
+				fw.write(names.get(i) + "\n");
+			}	
+				
+			
 				fw.close();
 			} catch (IOException e1) {
+				e1.printStackTrace();
 			}
-		} else if(e.getSource()==remove){
-			for (int i = 0; i < tet.length(); i++) {
-				try {
-					FileReader fr = new FileReader("src/intro_to_file_io/test4.txt");
-					int c = fr.read();
-					String newf = "";
-					boolean hasread = false;
-					while(c != -1){
-						if((char)c == '\n') {
-							hasread = true;
-							//c = fr.read();
-						}
-						//System.out.print((char)c);
-						c = fr.read();
-						
-					}
-					fr.close();
-				} catch (FileNotFoundException e1) {
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					e1.printStackTrace();
+		} else if (e.getSource() == load) {
+			String file = JOptionPane.showInputDialog("What is the file you want to load");
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				String line = br.readLine();
+				while(line != null) {
+					
+					names.add(line);
+					line = br.readLine();
 				}
 				
-				}
+				
+				br.close();
+			} catch (FileNotFoundException e1) {
+				
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				
+				e1.printStackTrace();
 			}
 		}
 	}
-
-
+}
